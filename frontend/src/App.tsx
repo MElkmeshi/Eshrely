7834e469b89eaa6f1be4a8ddae8f3cb359924b79
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2023-04-28 12:30:44 +0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+//import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <header style={{ backgroundColor: "black", padding: "1rem" }}>
+        <a href="/">Eshrely</a>
+      </header>
+      <main>list products</main>
+    </>
+  );
+}
+
+export default App;
