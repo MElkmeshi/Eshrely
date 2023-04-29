7faa355d3886e303f589639654a0382e2c9161b8
MElkmeshi <mohamed2108926@miuegypt.edu.eg>
2023-04-29 15:39:14 +0300

    added varient to message box

--- frontend/src/Components/MessageBox.tsx
+++ frontend/src/Components/MessageBox.tsx
@@ -1,8 +1,8 @@
 import Alert from "react-bootstrap/Alert";
 type Props = {
-  variant?: string;
+  variant: string;
   children: string | JSX.Element | JSX.Element[];
 };
 export default function MessageBox({ variant, children }: Props) {
-  return <Alert variant={"danger"}>{children}</Alert>;
+  return <Alert variant={variant || "info"}>{children}</Alert>;
 }
